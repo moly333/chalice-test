a2b33307ee2eb6c8a37573b17a114adf23db6a04
morimolymoly <morimolymoly@gmail.com>
2018-03-10 20:06:59 +0900

    iroiro

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
 from chalice import Chalice
+import json
+import boto3
+from botocore.exceptions import ClientError
+from chalice import NotFoundError
 
-app = Chalice(app_name='src')
+
+app = Chalice(app_name='chalice-test')
 
 
 @app.route('/')
@@ -12,9 +17,28 @@ def index():
 def view_prof(name):
     return {'name': name}
 
+
 @app.route('/introspect')
 def introspect():
     return app.current_request.to_dict()
+
+
+S3 = boto3.client('s3', 'ap-northeast-1')
+BUCKET = 'chalice-test-objects'
+
+
+@app.route('/objects/{key}', methods=['GET', 'PUT'])
+def s3objects(key):
+    request = app.current_request
+    if request.method == 'PUT':
+        S3.put_object(Bucket=BUCKET, Key=key,
+                      Body=json.dumps(request.json_body))
+    elif request.method == 'GET':
+        try:
+            response = S3.get_object(Bucket=BUCKET, Key=key)
+            return json.loads(response['Body'].read())
+        except ClientError as e:
+            raise NotFoundError(key)
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
